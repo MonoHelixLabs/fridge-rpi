b7d6b84cb0c4b8727c77fa4a5e611d86a5d1bcab
radudragusin <radudragusin@gmail.com>
2016-09-25 14:43:02 +0200

    Add support for Adafruit IO notification.

--- grab_images.py
+++ grab_images.py
@@ -3,25 +3,30 @@ import subprocess
 import os
 from timeit import default_timer as timer
 import boto3
+from Adafruit_IO import Client
 
+aio_key = os.environ['AIOKEY']
+aio = Client(aio_key)
 s3 = boto3.client('s3')
 size = "1920x720"
 dev0 = "/dev/video0"
 dev1 = "/dev/video1"
+mqtt_feed = 'new-feed'
+s3_bucket = 'myfridge.monohelixlabs.com'
 
 def grab_usb_image(dev, fname):
     print('grab_usb_image', dev, fname)
     fpath = os.path.join('/tmp', fname)
     p = subprocess.Popen(["fswebcam","-q","-d",dev,"--no-banner","-r",size,fpath])
     p.wait()
-    s3.upload_file(fpath,'myfridge',fname)
+    s3.upload_file(fpath, s3_bucket, fname)
 
 def grab_rpi_image(fname):
     print('grab_rpi_image', fname)
     fpath = os.path.join('/tmp', fname)
     p = subprocess.Popen(["raspistill","-w","1440","-h","720","-t","1","-o",fpath])
     p.wait()
-    s3.upload_file(fpath,'myfridge',fname)
+    s3.upload_file(fpath, s3_bucket, fname)
 
 def grab_images():
     start_time = timer()
@@ -32,4 +37,6 @@ def grab_images():
     pool.close()
     pool.join()
     end_time = timer()
-    print('Elapsed:',end_time - start_time)
+    delta_time = end_time - start_time
+    print('Elapsed:',delta_time)
+    aio.send(mqtt_feed, delta_time)
