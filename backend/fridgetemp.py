9b14548a93cf324e7a52d7be20e796ac32bac102
radudragusin <radudragusin@gmail.com>
2017-03-19 21:15:47 +0000

    Add temperature reader

---
+++ backend/fridgetemp.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from time import sleep
+from Adafruit_IO import Client
+from Adafruit_IO import MQTTClient
+
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+
+aio_key = os.environ['AIOKEY']
+aio_user = os.environ['AIOUSER']
+aio = Client(aio_key)
+
+feed_map = {
+    'tmptop':'28-011562093fff/w1_slave',
+    'tmpmiddle':'28-0115625258ff/w1_slave',
+    'tmpbottom':'28-0315622969ff/w1_slave'
+}
+
+def read_temp_raw(device_file):
+    f = open(os.path.join(base_dir,device_file), 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp(device_file):
+    lines = read_temp_raw(device_file)
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+if __name__ == '__main__':
+    for feed_name, device_file in feed_map:
+        temp_c = read_temp(device_file)
+        aio.send(feed_name, temp_c)
+    sleep(60)
