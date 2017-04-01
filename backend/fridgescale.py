0610c8dd18109526f44128f6a33108478816d505
petcupaula <petcu.paula@gmail.com>
2017-04-02 00:33:10 +0200

    Support for multiple scales

--- backend/fridgescale.py
+++ backend/fridgescale.py
@@ -11,12 +11,14 @@ def cleanAndExit():
     print("Bye!")
     sys.exit()
 
-hx = HX711(5, 6)
+hx1 = HX711(5, 6)
+hx2 = HX711(8,7)
 
 aio_key = os.environ['AIOKEY']
 aio = Client(aio_key)
 
-feed_name = 'scalepos2'
+feed_name1 = 'scalepos2'
+feed_name2 = 'scalepos3'
 
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, nump$
 # Still need to figure out why does it change.
@@ -25,7 +27,8 @@ feed_name = 'scalepos2'
 # The first parameter is the order in which the bytes are used to build the "long" value.
 # The second paramter is the order of the bits inside each byte.
 # According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
-hx.set_reading_format("LSB","MSB")
+hx1.set_reading_format("LSB","MSB")
+hx2.set_reading_format("LSB","MSB")
 
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
@@ -33,10 +36,13 @@ hx.set_reading_format("LSB","MSB")
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
 #hx.set_reference_unit(113)
-hx.set_reference_unit(-2037)
+hx1.set_reference_unit(-2037)
+hx2.set_reference_unit(-2037)
 
-hx.reset()
-hx.tare()
+hx1.reset()
+hx1.tare()
+hx2.reset()
+hx2.tare()
 
 while True:
     try:
@@ -48,15 +54,23 @@ while True:
         #print binary_string + " " + np_arr8_string
 
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        val = hx.get_weight(5)
-        print(val)
-        print(int(val))
-        aio.send(feed_name, int(val))
+        val1 = hx1.get_weight(5)
+        print("val1:" + str(val1))
+        #print(int(val1))
+        aio.send(feed_name1, int(val1))
 
-        hx.power_down()
-        hx.power_up()
+        hx1.power_down()
+        hx1.power_up()
+
+        val2 = hx2.get_weight(5)
+        print("val2:" + str(val2))
+        aio.send(feed_name2, int(val2))
+
+        hx2.power_down()
+        hx2.power_up()
         #time.sleep(0.5)
-        
+
         time.sleep(60)
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
+
