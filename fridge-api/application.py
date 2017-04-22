a96a555e4df4c10e843381f6e4f7ebb5ed22a66d
petcupaula <petcu.paula@gmail.com>
2017-04-22 11:49:48 +0200

    Fridge API: changed from MQTT client to REST client

--- fridge-api/application.py
+++ fridge-api/application.py
@@ -6,7 +6,6 @@
 
 import time
 from Adafruit_IO import Client
-from Adafruit_IO import MQTTClient
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 application = Flask(__name__)
@@ -14,37 +13,18 @@ cors = CORS(application)
 application.config['CORS_HEADERS'] = 'Content-Type'
 
 aio_key = ''
-aio_user = ''
 aio_rest = Client(aio_key)
-aio_mqtt = MQTTClient(aio_user, aio_key)
 mqtt_feed = 'fridgecam'
 mqtt_message = 'snapnow'
 min_sec_between_snaps = 30
 start_time = 0
 
-def connected(client):
-    print('Connected to AIO!')
-
-def disconnected(client):
-    print('Disconnected from AIO!')
-    mqtt_run()
-
-def setup_mqtt():
-    aio_mqtt.on_connect = connected
-    aio_mqtt.on_disconnect = disconnected
-
-def mqtt_run():
-    aio_mqtt.connect()
-    aio_mqtt.loop_background()
-
 def send_message():
     global start_time
     end_time = time.time()
     time_delta = end_time - start_time
-    print(start_time, end_time, time_delta)
     if time_delta > min_sec_between_snaps:
-        mqtt_run()
-        aio_mqtt.publish(mqtt_feed, mqtt_message)
+        aio_rest.send(mqtt_feed, mqtt_message)
         start_time = end_time
         return "Request sent"
     return "Too soon, wait " + str(int(min_sec_between_snaps - time_delta)) + " sec"
@@ -76,6 +56,4 @@ def milkstatus():
     return jsonify({'status':{'liters':str(liters)}})
 
 if __name__ == "__main__":
-    setup_mqtt()
-    mqtt_run()
     application.run()
